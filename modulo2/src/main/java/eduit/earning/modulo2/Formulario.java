6788d110d9d1d6806f89399220f7d20a820a47c7
Abraham Chávez Castillo <abraham_cha88@hotmail.com>
2021-01-21 17:10:29 -0600

    Se agrega objetos vistos en clase 21 de enero de 2021 - MVVM

--- modulo2/src/main/java/eduit/earning/modulo2/Formulario.java
+++ modulo2/src/main/java/eduit/earning/modulo2/Formulario.java
@@ -43,6 +43,8 @@ public class Formulario extends javax.swing.JFrame {
         btnEliminar = new javax.swing.JButton();
         btnGuardar = new javax.swing.JButton();
         btnCancelar = new javax.swing.JButton();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        lstEmpleados = new javax.swing.JList<>();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Captura de empleado");
@@ -150,6 +152,9 @@ public class Formulario extends javax.swing.JFrame {
             }
         });
 
+        lstEmpleados.setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
+        jScrollPane2.setViewportView(lstEmpleados);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -178,9 +183,7 @@ public class Formulario extends javax.swing.JFrame {
                                     .addComponent(lblDepartamento))
                                 .addGap(18, 18, 18)
                                 .addComponent(pnlGeneros, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(txtDepartamento, javax.swing.GroupLayout.PREFERRED_SIZE, 161, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(44, 44, 44))))
+                            .addComponent(txtDepartamento, javax.swing.GroupLayout.PREFERRED_SIZE, 161, javax.swing.GroupLayout.PREFERRED_SIZE)))
                     .addComponent(lblObservaciones)
                     .addComponent(jScrollPane1)
                     .addGroup(layout.createSequentialGroup()
@@ -193,45 +196,50 @@ public class Formulario extends javax.swing.JFrame {
                         .addComponent(btnGuardar, javax.swing.GroupLayout.PREFERRED_SIZE, 73, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(btnCancelar)))
+                .addGap(18, 18, 18)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 165, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(pnlGeneros, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(lblNombre)
-                            .addComponent(lblApellidos)
-                            .addComponent(lblEdad))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(pnlGeneros, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                    .addComponent(lblNombre)
+                                    .addComponent(lblApellidos)
+                                    .addComponent(lblEdad))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                        .addComponent(txtNombre, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(txtApellidos, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                    .addComponent(spnEdad, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addGap(18, 18, 18)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                    .addComponent(lblEmail)
+                                    .addComponent(lblDepartamento))))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                                .addComponent(txtNombre, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addComponent(txtApellidos, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addComponent(spnEdad, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(txtEmail, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(txtDepartamento, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(lblObservaciones)
+                        .addGap(18, 18, 18)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 96, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(18, 18, 18)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(lblEmail)
-                            .addComponent(lblDepartamento))))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(txtEmail, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(txtDepartamento, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(lblObservaciones)
-                .addGap(18, 18, 18)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 96, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(btnNuevo)
-                    .addComponent(btnModificar)
-                    .addComponent(btnEliminar)
-                    .addComponent(btnGuardar)
-                    .addComponent(btnCancelar))
-                .addContainerGap(23, Short.MAX_VALUE))
+                            .addComponent(btnNuevo)
+                            .addComponent(btnModificar)
+                            .addComponent(btnEliminar)
+                            .addComponent(btnGuardar)
+                            .addComponent(btnCancelar)))
+                    .addComponent(jScrollPane2))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         lblNombre.getAccessibleContext().setAccessibleName("lblNombre");
@@ -299,12 +307,14 @@ public class Formulario extends javax.swing.JFrame {
     public javax.swing.JButton btnModificar;
     public javax.swing.JButton btnNuevo;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JLabel lblApellidos;
     private javax.swing.JLabel lblDepartamento;
     private javax.swing.JLabel lblEdad;
     private javax.swing.JLabel lblEmail;
     private javax.swing.JLabel lblNombre;
     private javax.swing.JLabel lblObservaciones;
+    public javax.swing.JList<String> lstEmpleados;
     private javax.swing.JPanel pnlGeneros;
     public javax.swing.JRadioButton rbnHombre;
     public javax.swing.JRadioButton rbnMujer;
--- modulo2/src/main/java/eduit/earning/modulo2/viewmodel/FormularioViewModel.java
+++ modulo2/src/main/java/eduit/earning/modulo2/viewmodel/FormularioViewModel.java
@@ -11,10 +11,14 @@ import eduit.learning.modulo2.model.Empleado;
 import eduit.learning.modulo2.model.Generos;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import javax.swing.DefaultListModel;
+import javax.swing.JList;
 import javax.swing.JOptionPane;
 
 /**
@@ -22,83 +26,86 @@ import javax.swing.JOptionPane;
  * @author abraham
  */
 public class FormularioViewModel {
-    
+
     private Formulario view;
     private List<Empleado> listaEmpleados;
-    
+
     public FormularioViewModel(Formulario view) {
         this.view = view;
-        
+
         InicializarEventos();
-        
+
         LimpiarComponentes();
         HabilitarDeshabilitarComponentes(false);
         MostrarOcultarBotonesPrincipales(true);
         MostrarOcultarBotonesSecundarios(false);
-        
+
         listaEmpleados = new ArrayList();
+
+        this.LeerArchivo();
+        this.LlenarListaEmpleadosVista();
     }
-    
+
     private void InicializarEventos() {
         this.view.btnNuevo.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 AccionNuevo();
             }
-            
+
         });
-        
+
         this.view.btnModificar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 AccionModificar();
             }
-            
+
         });
-        
+
         this.view.btnEliminar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 AccionEliminar();
             }
-            
+
         });
-        
+
         this.view.btnGuardar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 AccionGuardar();
             }
-            
+
         });
-        
+
         this.view.btnCancelar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 AccionCancelar();
             }
-            
+
         });
     }
-    
+
     private void AccionNuevo() {
         LimpiarComponentes();
         MostrarOcultarBotonesPrincipales(false);
         MostrarOcultarBotonesSecundarios(true);
         HabilitarDeshabilitarComponentes(true);
     }
-    
+
     private void AccionModificar() {
-        
+
     }
-    
+
     private void AccionEliminar() {
-        
+
     }
-    
+
     private void AccionGuardar() {
         Empleado emp = new Empleado();
-        
+
         emp.setNombre(this.view.txtNombre.getText());
         emp.setApellidos(this.view.txtApellidos.getText());
         emp.setDepartamento(this.view.txtDepartamento.getText());
@@ -108,9 +115,9 @@ public class FormularioViewModel {
                 : this.view.rbnMujer.isSelected() == true ? Generos.Mujer
                 : Generos.Otro);
         emp.setEdad(Byte.parseByte(this.view.spnEdad.getValue().toString()));
-        
+
         this.listaEmpleados.add(emp);
-        
+
         boolean guardadoExitoso = this.GuardarEnArchivo();
         if (guardadoExitoso == true) {
             LimpiarComponentes();
@@ -118,13 +125,13 @@ public class FormularioViewModel {
             MostrarOcultarBotonesSecundarios(false);
             HabilitarDeshabilitarComponentes(false);
         }
-        
+
     }
-    
+
     private void AccionCancelar() {
         var result = JOptionPane.showConfirmDialog(this.view, "¿Está seguro que desea cancelar la operación?", "Cancelar operación",
                 JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
-        
+
         if (result == JOptionPane.YES_OPTION) {
             LimpiarComponentes();
             MostrarOcultarBotonesPrincipales(true);
@@ -132,52 +139,87 @@ public class FormularioViewModel {
             HabilitarDeshabilitarComponentes(false);
         }
     }
-    
+
     private void MostrarOcultarBotonesPrincipales(boolean estanVisibles) {
         this.view.btnNuevo.setVisible(estanVisibles);
         this.view.btnModificar.setVisible(estanVisibles);
         this.view.btnEliminar.setVisible(estanVisibles);
     }
-    
+
     private void MostrarOcultarBotonesSecundarios(boolean estanVisibles) {
         this.view.btnGuardar.setVisible(estanVisibles);
         this.view.btnCancelar.setVisible(estanVisibles);
     }
-    
+
     private void HabilitarDeshabilitarComponentes(boolean estanHabilitados) {
         this.view.txtNombre.setEnabled(estanHabilitados);
         this.view.txtApellidos.setEnabled(estanHabilitados);
         this.view.txtDepartamento.setEnabled(estanHabilitados);
         this.view.txtEmail.setEnabled(estanHabilitados);
         this.view.txtObservaciones.setEnabled(estanHabilitados);
-        
+
         this.view.spnEdad.setEnabled(estanHabilitados);
         this.view.rbnHombre.setEnabled(estanHabilitados);
         this.view.rbnMujer.setEnabled(estanHabilitados);
         this.view.rbnOtro.setEnabled(estanHabilitados);
     }
-    
+
     private void LimpiarComponentes() {
         this.view.txtNombre.setText("");
         this.view.txtApellidos.setText("");
         this.view.txtDepartamento.setText("");
         this.view.txtEmail.setText("");
         this.view.txtObservaciones.setText("");
-        
+
         this.view.spnEdad.setValue(18);
         this.view.btgGeneros.clearSelection();
     }
-    
+
+    private void LlenarListaEmpleadosVista() {
+        DefaultListModel listModel = new DefaultListModel();
+
+        for (int i = 0; i < this.listaEmpleados.size(); i++) {
+            listModel.add(i, this.listaEmpleados.get(i).getNombre() + " " + this.listaEmpleados.get(i).getApellidos());
+        }
+
+        this.view.lstEmpleados.setModel(listModel);
+    }
+
+    private void LeerArchivo() {
+        try {
+            FileReader fr = new FileReader("C:\\TMP\\Formulario_ListaEmpleados.txt");
+            StringBuilder sb = new StringBuilder();
+
+            //String cadena = "Hola";
+            //H, o, l, a
+
+            int c = 0;
+            while ((c = fr.read()) != -1) {
+                sb.append((char) c);
+                //cadena = cadena + c;
+                //H, o, l, a, r, t, y, u, t, w, 
+            }
+
+            fr.close();
+
+            Gson gson = new Gson();
+            this.listaEmpleados = Arrays.asList(gson.fromJson(sb.toString(), Empleado[].class));
+
+        } catch (IOException ex) {
+
+        }
+    }
+
     private boolean GuardarEnArchivo() {
         try {
             FileWriter fw = new FileWriter("C:\\TMP\\Formulario_ListaEmpleados.txt");
-            
+
             Gson gson = new Gson();
             String listaEmpleadosJson = gson.toJson(this.listaEmpleados);
-            
+
             fw.write(listaEmpleadosJson);
             fw.close();
-            
+
             return true;
         } catch (IOException ex) {
             var result = JOptionPane.showConfirmDialog(this.view, "¡Ocurrió un error al intentar guardar el archivo!", "Error",
