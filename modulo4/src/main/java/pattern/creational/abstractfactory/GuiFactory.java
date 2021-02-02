60a8927a5955a6d01712db871ba1d79951e8ca7d
Abraham Chávez Castillo <abraham_cha88@hotmail.com>
2021-02-02 10:58:46 -0600

    Se agrega objetos vistos en clase 02 de febrero de 2021

--- modulo4/src/main/java/pattern/creational/abstractfactory/GuiFactory.java
+++ modulo4/src/main/java/pattern/creational/abstractfactory/GuiFactory.java
@@ -8,4 +8,10 @@ public interface GuiFactory {
     Button createButton();
 
     Panel createPanel();
+    
+    void createTable();
+    
+    void createLabel();
+    
+    void createTextField();
 }
