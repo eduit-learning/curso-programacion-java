9dc2a75a45928c8c1a9b2a79a95e683732cde5df
Abraham Chávez Castillo <abraham_cha88@hotmail.com>
2021-02-09 11:19:01 -0600

    Se agrega objetos vistos en clase 09 de febrero de 2021

--- EjemploPropertyChange_Ant/src/eduit/learning/demo/ejemplopropertychange/PropertyChangedViewModel.java
+++ EjemploPropertyChange_Ant/src/eduit/learning/demo/ejemplopropertychange/PropertyChangedViewModel.java
@@ -31,13 +31,17 @@ public class PropertyChangedViewModel {
                 DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss");
                 LocalDateTime now = LocalDateTime.now();
                 model.setHora(dtf.format(now));
+
+                if (milisegundos % 1000 == 0) {
+                    model.setCronometroSegundos("" + (milisegundos / 1000));
+                }
             }
         };
 
         this.milisegundos = 0;
 
         // Aquí se pone en marcha el timer cada segundo.
-        // Dentro de 0 milisegundos avísame cada 1000 milisegundos
+        // Dentro de 0 milisegundos avísame cada 100 milisegundos
         this.timerCronometro.scheduleAtFixedRate(timerTask, 0, 100);
     }
 
